8ca6c8961ab26687d93c7babac51ec9d78bce7ff
YD <yahordunayeu@gmail.com>
2021-02-22 17:10:51 +0100

    some more methods

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
 import $ from './lib/lib';
 
-$('button').on('click', function() {
-    $(this).addGetAttribute('value');
+/* $('button').on('click', function() {
+    $('div').eq(2).toggleClass('active');
 });
+
+$('div').click(function () {
+    console.log($(this).index());
+});
+
+console.log($('.some').closest('.findme')); */
+
+console.log($('.findme').siblings());
+
 
